a15031886f5ee55b1e413c01e969c80f66539a9c
edda2022 <candyreinecke@googlemail.com>
2023-01-04 17:25:08 +0100

    chenges we did together

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Routes, Route, Link, useNavigate, NavLink} from 'react-router-dom';
 import Recipedetail from './components/Recipe/Recipedetail';
 
 import Header from './components/header/Header'
-import Home from './components/header/Home'
+import Home from './components/Home/Home'
 import Ingredients from './components/header/Ingredients'
 import Cuisine from './components/header/Cuisine'
 import Register from './components/header/Register'
@@ -13,36 +13,13 @@ import Login from './components/header/Login'
 
 function App() {
 
-  const [recipes, setRecipes] = useState([])
-
-
-  //call the Contentful-client to fetch the data
-  const contentful = require('contentful')
-  const client = contentful.createClient({
-    space: '3nafpp0jo6h4',
-    environment: 'master', // defaults to 'master' if not set
-    accessToken: 'hsNzkIL8Lrero_6ljmPQHYT7gn9_0sho0Akw6R7tQ_s'
-  })
-
-  useEffect(() =>{
-  client.getEntries()
-  .then((response) => {
-    console.log(response.items)
-    setRecipes(response.items)})
-  .catch(console.error)
-  }, [])
 
 
   return (
     <>
     <div className="App">
       <h1 className="title">Easy Peasy Recipes</h1>
-            <Header />
-    <div className="recipes">
-    <Recipedetail />
-      {recipes?.map((recipe) => {
-        return <Recipes recipe={recipe} key={recipe.sys.id} /> })}
-    </div>
+      <Header />
     <Routes>
       <Route path='/home' element={<Home />}></Route>
       <Route path="/recipes" element={<Recipes />}></Route>
--- src/components/Recipe/Recipes.js
+++ src/components/Recipe/Recipes.js
@@ -1,10 +1,34 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 
 export default function Recipes ({recipe}) {
 
+    const [recipes, setRecipes] = useState([])
+
+
+  //call the Contentful-client to fetch the data
+  const contentful = require('contentful')
+  const client = contentful.createClient({
+    space: '3nafpp0jo6h4',
+    environment: 'master', // defaults to 'master' if not set
+    accessToken: 'hsNzkIL8Lrero_6ljmPQHYT7gn9_0sho0Akw6R7tQ_s'
+  })
+
+  useEffect(() =>{
+  client.getEntries()
+  .then((response) => {
+    console.log(response.items)
+    setRecipes(response.items)})
+  .catch(console.error)
+  }, [])
+
+
 
             return (
+        <div className="recipes">
+            {recipes?.map((recipe) => {
+            return  (
             <div className="recipe">
                  <div className="imagediv">
                     {recipe.fields.recipeImages?.map((image) => {
@@ -21,6 +45,9 @@ export default function Recipes ({recipe}) {
                 <button>View More</button>
                 </Link>
                
+            </div>
+                        )})}
+
             </div>
             )
     
